a990f50d12f41cb99be62021a819344cddef513c
GrunCrow <57197254+GrunCrow@users.noreply.github.com>
2022-12-19 11:31:48 +0100

    TicTacToe v0.5. Display Board Functions added and working

--- Code/Game.hs
+++ Code/Game.hs
@@ -24,6 +24,8 @@
 -- win = 3
 -- depth = ?
 
+import Data.List
+
 -- ============================================================================
 --										TicTacToe Objects
 -- ============================================================================
@@ -83,3 +85,53 @@ wins player board = any line (rows ++ cols ++ diags)
 won :: Board -> Bool
 won board = wins O board || wins X board
 
+-- ============================================================================
+--										Display Board
+-- ============================================================================
+
+-- tabulation: tabulates values between values of the list, for screen showing the board
+tabulation :: a -> [a] -> [a]
+tabulation x [] = []
+tabulation x [y] = [y]
+tabulation x (y:ys) = y : x : tabulation x ys
+
+-- showPlayer: converts player value to alist of strings
+showPlayer :: Player -> [String]
+showPlayer O = ["   ", " O ", "   "]
+showPlayer X = ["   ", " X ", "   "]
+showPlayer B = ["   ", "   ", "   "]
+
+-- unlines: creates a string from a list of strings, it inserts new line characters between original strings
+unlines_fun :: [String] -> String
+unlines_fun [] = ""
+unlines_fun (x:xs) = x ++ "\n" ++ unlines_fun xs
+
+-- foldr1: apply the function to the items of the list from 2 to 2 starting from left
+foldr_fun :: (a -> a -> a) -> [a] -> a
+foldr_fun _ [x] = x
+foldr_fun f (x:xs) = f x (foldr_fun f xs)
+-- foldr_fun (+) [1,2,3] -> 6
+
+showRow :: [Player] -> [String]
+showRow = beside . tabulation bar . map showPlayer
+			where
+				beside = foldr_fun (zipWith (++))
+				bar = replicate 3 "|"
+-- showRow [B,X,O] -> ["   |   |   ","   | X | O ","   |   |   "]
+
+-- displays the Board given 
+putBoard :: Board -> IO ()
+putBoard = putStrLn . unlines_fun . concat . tabulation bar . map showRow
+			where bar = [replicate ((size*4)-1) '-']
+-- putBoard [[O,X,O],[X,B,O],[B,B,B]]
+--   |   |   
+-- O | X | O 
+--   |   |   
+-------------
+--   |   |   
+-- X |   | O 
+--   |   |   
+-------------
+--   |   |   
+--   |   |   
+--   |   |   
