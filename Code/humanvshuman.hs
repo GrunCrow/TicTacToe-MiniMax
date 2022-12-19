5a734c8ac709934b15ed1fe9712e55133ea4cf0c
GrunCrow <57197254+GrunCrow@users.noreply.github.com>
2022-12-19 13:26:06 +0100

    TicTacToe v2.0. MiniMax Version

--- Code/humanvshuman.hs
+++ Code/humanvshuman.hs
@@ -26,6 +26,7 @@
 
 import Data.Char
 import Data.List
+import System.IO
 
 -- ============================================================================
 --										TicTacToe Objects
@@ -192,8 +193,7 @@ goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
 -- checks if any player has won or if board is full. if not if calls getnat to ask for next move, 
 -- if move is valid then it performs the move if not error and rerun run with same parameters
 run' :: Board -> Player -> IO ()
-run' board player 
-		 | wins O board = putStrLn "Jugador O gana!\n"
+run' board player | wins O board = putStrLn "Jugador O gana!\n"
          | wins X board = putStrLn "Jugador X gana!\n"
          | full board   = putStrLn "Empate!\n"
          | otherwise = 
@@ -205,73 +205,3 @@ run' board player
 				  
 humanvshuman :: IO ()
 humanvshuman = run empty O
-
-
--- ============================================================================
---											GameTree
--- ============================================================================
-
-data Tree a = Node a [Tree a] deriving Show
-
-gametree :: Board -> Player -> Tree Board
-gametree board player = Node board [gametree board' (next player) | board' <- moves board player]
-
-moves :: Board -> Player -> [Board]
-moves board player
-	| won board = []
-	| full board = []
-	| otherwise = concat [move board position player | position <- [0..((size^2)-1)]]
-
-prune :: Int -> Tree a -> Tree a
-prune 0 (Node x _) = Node x []
-prune n (Node x ts) = Node x [prune (n-1) t | t <- ts]
-	
-depth :: Int
-depth = 4
-
--- minimax: takes possible tree and returns another tree that have the winning player label 
-minimax :: Tree Board -> Tree (Board, Player)
-minimax (Node board [])
-	| wins O board = Node (board, O) []
-	| wins X board = Node (board, X) []
-	| otherwise = Node (board, B) []
-minimax (Node board ts)
-	| turn board == O = Node (board, minimum ps) ts'
-	| turn board == X = Node (board, maximum ps) ts'
-						where
-							ts' = map minimax ts
-							ps = [p | Node (_,p) _ <- ts']
-						
--- gives best move given a board and a player						
-bestmoves :: Board -> Player -> Board
-bestmoves board player = head [board' | Node (board', player') _ <- ts, player' == best]
-							where
-								tree = prune depth (gametree board player)
-								Node (_,best) ts = minimax tree
-								
--- ============================================================================
---											Run the Game
--- ============================================================================
-
-play :: Board -> Player -> IO()
-play board player = do --cls
-						goto (1,1)
-						putBoard board
-						play' board player
-
-
-							
-play' :: Board -> Player -> IO ()
-play' board player 
-		| wins O board = putStrLn "Jugador O gana!\n"
-        | wins X board = putStrLn "Jugador X gana!\n"
-        | full board   = putStrLn "Empate!\n"
-		| player == O = do 
-						position <- getNat (prompt player)
-						case move board position player of
-							[] -> do play' board player	-- putStrLn "Error: Movimiento no valido"
-							[board'] -> play board' (next player)
-        | player == X = do (play $! (bestmoves board player)) (next player) -- putStrLn "Jugador X esta pensando..."
-		
-main :: IO ()
-main = do play empty O		
